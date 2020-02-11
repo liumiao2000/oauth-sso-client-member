67d8fcb615202f3172403e552f949d4894a4ee93
liumiao2000 <liu_2000_112@163.com>
2020-02-11 23:08:50 +0800

    初始导入

---
+++ src/main/java/com/oauthSSOClient/member/OauthSsoClientMemberApplication.java
@@ -0,0 +1,13 @@
+package com.oauthSSOClient.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSsoClientMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSsoClientMemberApplication.class, args);
+	}
+
+}
